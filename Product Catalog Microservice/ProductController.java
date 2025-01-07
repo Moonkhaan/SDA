73d2a0e7e198e882b182e80f48d170e02703a243
Muneeb Khan <135807014+Moonkhaan@users.noreply.github.com>
2025-01-07 12:59:40 +0500

    Add files via upload

---
+++ Product Catalog Microservice/ProductController.java
@@ -0,0 +1,30 @@
+package com.amazon.microservice.controller;
+
+import com.amazon.microservice.model.Product;
+import com.amazon.microservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/category/{category}")
+    public List<Product> getProductsByCategory(@PathVariable String category) {
+        return productService.getProductsByCategory(category);
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+}
